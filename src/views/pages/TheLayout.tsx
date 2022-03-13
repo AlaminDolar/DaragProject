7dcb8c7f447c385239873d4ac3af2c08b66c3a4a
AlaminDolar <amin15-9898@diu.edu.bd>
2022-03-13 11:44:35 +0600

    first commit

---
+++ src/views/pages/TheLayout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TheLayout = () => {
+    return (
+        <div>
+            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima repudiandae nobis quod natus tenetur facilis sapiente corrupti ipsa eligendi officia quam rerum asperiores earum expedita in accusantium, alias commodi dignissimos.</h2>
+        </div>
+    );
+};
+
+export default TheLayout;
+
